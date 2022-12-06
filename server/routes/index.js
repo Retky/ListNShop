d30f520d5e3608c7fd8b006c62522f290dac75bc
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2022-12-05 20:30:56 -0600

    Add 'items routes' CRUD for user

--- server/routes/index.js
+++ server/routes/index.js
@@ -54,4 +54,27 @@ router
     res.json({ Message: `Delete list with id: ${req.params.listId}` });
   });
 
+// Item Routes
+router
+  .route('/users/:id/lists/:listId/items')
+  .get((req, res) => {
+    res.json({ Message: `List of all items for list with id: ${req.params.listId}` });
+  })
+  .post((req, res) => {
+    console.log(req.body);
+    res.json({ Message: 'Successfully created a new item' });
+  });
+router
+  .route('/users/:id/lists/:listId/items/:itemId')
+  .get((req, res) => {
+    res.json({ Message: `Item with id: ${req.params.itemId}` });
+  })
+  .put((req, res) => {
+    console.log(req.body);
+    res.json({ Message: `Update item with id: ${req.params.itemId}` });
+  })
+  .delete((req, res) => {
+    res.json({ Message: `Delete item with id: ${req.params.itemId}` });
+  });
+
 module.exports = router;
