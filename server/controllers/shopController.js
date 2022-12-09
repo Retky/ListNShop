e31fa36f83a5970065e36bfcc3eabe06b9c0f06d
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2022-12-09 14:52:22 -0600

    Mod 'shopController' to individual file

---
+++ server/controllers/shopController.js
@@ -0,0 +1,29 @@
+const getShops = (req, res) => {
+  res.send(`Get all shops for user ${req.params.userId}`);
+};
+
+const getShopById = (req, res) => {
+  res.send(`Get shop ${req.params.shopId} for user ${req.params.userId}`);
+};
+
+const createShop = (req, res) => {
+  console.log(req.body);
+  res.send(`Create new shop for user ${req.params.userId}`);
+};
+
+const updateShop = (req, res) => {
+  console.log(req.body);
+  res.send(`Update shop ${req.params.shopId} for user ${req.params.userId}`);
+};
+
+const deleteShop = (req, res) => {
+  res.send(`Delete shop ${req.params.shopId} for user ${req.params.userId}`);
+};
+
+module.exports = {
+  getShops,
+  getShopById,
+  createShop,
+  updateShop,
+  deleteShop,
+};
--- server/v1/routes/shopRoutes.js
+++ server/v1/routes/shopRoutes.js
@@ -1,28 +1,14 @@
 const { Router } = require('express');
 
 const router = Router();
+const shopController = require('../../controllers/shopController.js');
 
 // Shops Routes
 router
-  .route('/')
-  .get((req, res) => {
-    res.json({ Message: `List of all shops for user with id: ${req.params.id}` });
-  })
-  .post((req, res) => {
-    console.log(req.body);
-    res.json({ Message: 'Successfully created a new shop' });
-  });
-router
-  .route('/:shopId')
-  .get((req, res) => {
-    res.json({ Message: `Shop with id: ${req.params.shopId}` });
-  })
-  .put((req, res) => {
-    console.log(req.body);
-    res.json({ Message: `Update shop with id: ${req.params.shopId}` });
-  })
-  .delete((req, res) => {
-    res.json({ Message: `Delete shop with id: ${req.params.shopId}` });
-  });
+  .get('/', shopController.getShops)
+  .get('/:shopId', shopController.getShopById)
+  .post('/', shopController.createShop)
+  .put('/:shopId', shopController.updateShop)
+  .delete('/:shopId', shopController.deleteShop);
 
 module.exports = router;
