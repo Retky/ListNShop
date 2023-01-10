669f2dc536c4a22639b79488d191063fb42de584
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2023-01-09 21:54:34 -0600

    Add 'Get ListItems' from DB

--- server/controllers/listItemsController.js
+++ server/controllers/listItemsController.js
@@ -8,10 +8,22 @@ const pool = new Pool({
   port: '5432',
 });
 
+const getAllListItems = async (req, res) => {
+  const { listId } = req.params;
+  const response = await pool.query('SELECT * FROM list_items WHERE list_id = $1', [listId]);
+  res.status(200).json(response.rows);
+};
+
+const getListItemById = async (req, res) => {
+  const { itemId } = req.params;
+  const response = await pool.query('SELECT * FROM list_items WHERE item_id = $1', [itemId]);
+  res.status(200).json(response.rows);
+};
+
 module.exports = {
-  getAlllist,
-  getListById,
-  createList,
-  updateList,
-  deleteList,
+  getAllListItems,
+  getListItemById,
+  createListItem,
+  updateListItem,
+  deleteListItem,
 };
