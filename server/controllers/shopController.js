8ae48cd0cc27483745fd43cef6e7b9e252e72abc
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2023-01-10 13:38:27 -0600

    Mod 'shopcontrollers' to interact with DB

--- server/controllers/shopController.js
+++ server/controllers/shopController.js
@@ -1,23 +1,40 @@
-const getAllShops = (req, res) => {
-  res.send(`Get all shops for user ${req.params.userId}`);
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: 'localhost',
+  user: 'node',
+  password: 'fox',
+  database: 'listnshop',
+  port: '5432',
+});
+
+const getAllShops = async (req, res) => {
+  const response = await pool.query('SELECT * FROM shops WHERE user_id = $1', [req.params.userId]);
+  res.status(200).json(response.rows);
 };
 
-const getShopById = (req, res) => {
-  res.send(`Get shop ${req.params.shopId} for user ${req.params.userId}`);
+const getShopById = async (req, res) => {
+  const response = await pool.query('SELECT * FROM shops WHERE id = $1 AND user_id = $2', [req.params.shopId, req.params.userId]);
+  res.status(200).json(response.rows);
 };
 
-const createShop = (req, res) => {
+const createShop = async (req, res) => {
   console.log(req.body);
-  res.send(`Create new shop for user ${req.params.userId}`);
+  const { name } = req.body;
+  await pool.query('INSERT INTO shops (name, user_id) VALUES ($1, $2)', [name, req.params.userId]);
+  res.send(`Shop ${name} created for user ${req.params.userId}`);
 };
 
-const updateShop = (req, res) => {
+const updateShop = async (req, res) => {
   console.log(req.body);
-  res.send(`Update shop ${req.params.shopId} for user ${req.params.userId}`);
+  const { name } = req.body;
+  await pool.query('UPDATE shops SET name = $1 WHERE id = $2 AND user_id = $3', [name, req.params.shopId, req.params.userId]);
+  res.send(`Shop ${req.params.shopId} updated for user ${req.params.userId}`);
 };
 
-const deleteShop = (req, res) => {
-  res.send(`Delete shop ${req.params.shopId} for user ${req.params.userId}`);
+const deleteShop = async (req, res) => {
+  await pool.query('DELETE FROM shops WHERE id = $1 AND user_id = $2', [req.params.shopId, req.params.userId]);
+  res.send(`Shop ${req.params.shopId} deleted for user ${req.params.userId}`);
 };
 
 module.exports = {
