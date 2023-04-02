d3eece3a632c822faddd9afcf491c3db4b44e442
Retky <jose.arredondo.a@outlook.com>
2023-04-01 19:26:56 -0600

    Add 'Fetch local items' by list

--- client/src/redux/configureStore.js
+++ client/src/redux/configureStore.js
@@ -1,11 +1,13 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import lists from './lists';
 import stores from './stores';
+import listItems from './listItems';
 
 const store = configureStore({
   reducer: combineReducers({
     lists,
     stores,
+    listItems,
   }),
 });
 
---
+++ client/src/redux/listItems.js
@@ -0,0 +1,24 @@
+const FETCH_LOCAL_LIST_ITEMS = 'FETCH_LOCAL_LIST_ITEMS';
+
+const initialState = [];
+
+export const fetchLocalListItems = (listId) => {
+  console.log(listId);
+  const listItems = JSON.parse(localStorage.getItem('items')) || initialState;
+  const filteredListItems = listItems.filter((item) => item.list_id === listId);
+  return {
+    type: FETCH_LOCAL_LIST_ITEMS,
+    payload: filteredListItems,
+  };
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_LOCAL_LIST_ITEMS:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default reducer;
