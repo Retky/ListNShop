0c42ba0629ea80481af741c60ac7b55c47a509eb
Retky <jose.arredondo.a@outlook.com>
2023-04-16 06:19:21 -0600

    Add 'shops' list

--- client/src/pages/shops.jsx
+++ client/src/pages/shops.jsx
@@ -1,9 +1,26 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchLocalShops } from "../redux/shops";
+
 const Shops = () => {
-  return (
+  const dispatch = useDispatch();
+  const shops = useSelector((state) => state.shops);
+
+  useEffect(() => {
+    dispatch(fetchLocalShops());
+  }, [dispatch]);
+
+  const page = (
     <div>
       <h1>Shops</h1>
+      <ul>
+        {shops.map((shop) => (
+          <li key={shop.id}>{shop.name}</li>
+        ))}
+      </ul>
     </div>
   );
+  return page;
 };
 
 export default Shops;
