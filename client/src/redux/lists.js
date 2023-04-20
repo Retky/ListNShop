e8af80f9750044297cdd7d1bae950c9c019583c6
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2023-04-20 17:43:00 -0600

    Mod 'fetchLists' to use new state

--- client/src/redux/lists.js
+++ client/src/redux/lists.js
@@ -1,11 +1,16 @@
 const FETCH_LOCAL_LISTS = 'FETCH_LOCAL_LISTS';
-const initialState = [{ id: 0, name: 'New List' }];
+const initialState = {
+  nextId: 1,
+  lists: [
+    { id: 0, name: 'New List' }
+  ],
+};
 
 export const fetchLocalLists = () => {
-  const lists = JSON.parse(localStorage.getItem('lists')) || initialState;
+   const fetch = JSON.parse(localStorage.getItem('lists')) || initialState;
   return {
     type: FETCH_LOCAL_LISTS,
-    payload: lists,
+    payload: fetch.lists,
   };
 };
 
