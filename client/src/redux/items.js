fd3539e82cc34df92f073aa7e2f925230095b690
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2023-04-21 16:58:53 -0600

    Add 'delLocalItem' reduer

--- client/src/redux/items.js
+++ client/src/redux/items.js
@@ -3,6 +3,7 @@ const UPDATE_LOCAL_ITEM_QUANTITY = 'UPDATE_LOCAL_ITEM_QUANTITY';
 const INC_LOCAL_ITEM_QUANTITY = 'INC_LOCAL_ITEM_QUANTITY';
 const DEC_LOCAL_ITEM_QUANTITY = 'DEC_LOCAL_ITEM_QUANTITY';
 const ADD_LOCAL_ITEM = 'ADD_LOCAL_ITEM';
+const DELETE_LOCAL_ITEM = 'DELETE_LOCAL_ITEM';
 const initialState = {
   nextId: 1,
   items: [],
@@ -67,6 +68,18 @@ export const addLocalItem = (itemName, itemQuantity, itemUnit) => {
     itemId: newItem.id,
   };
 };
+export const deleteLocalItem = (itemId) => {
+  const fetch = JSON.parse(localStorage.getItem('items')) || initialState;
+  const items = fetch.items;
+  const item = items.find((item) => item.id === itemId);
+  const index = items.indexOf(item);
+  items.splice(index, 1);
+  localStorage.setItem('items', JSON.stringify(fetch));
+  return {
+    type: DELETE_LOCAL_ITEM,
+    payload: items,
+  };
+};
 
 const reducer = (state = initialState.items, action) => {
   switch (action.type) {
@@ -79,6 +92,8 @@ const reducer = (state = initialState.items, action) => {
     case DEC_LOCAL_ITEM_QUANTITY:
       return action.payload;
     case ADD_LOCAL_ITEM:
+      return action.payload;
+    case DELETE_LOCAL_ITEM:
       return action.payload;
     default:
       return state;
